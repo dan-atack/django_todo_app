f16fa4760799b1f67aeed5cf1fe9933622a05f54
dan-atack <dan_atack@hotmail.com>
2020-07-27 23:36:57 -0400

    database items on parade

--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import ToDoList
 
 # Create your views here.
 
 
 def index(req):
-    return render(req, "jobs/index.html")
+
+    todo_items = ToDoList.objects.order_by("id")
+    context = {"todo_items": todo_items}
+    return render(req, "jobs/index.html", context)
 
